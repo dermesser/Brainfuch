ab334b348c9cca4ee75e10d537a94c22b5a3a5f1
lbo <lbo@spheniscida.de>
2013-11-16 23:24:23 +0100

    Applied some HLint hints; created live debugger
    The live debugger plays the stack states on the screen.

--- BFdbgIA.hs
+++ BFdbgIA.hs
@@ -15,30 +15,42 @@ main = do
         putStr "\nOutput of script:\n"
         stacks <- interpret code
         putStr "\n\nStack states during interpretation:\n"
-        putStrLn $ showStacks stacks
-
--- Constants
+        mapM_ showWaitDelete (zipWith appendRotateSym (stackStrings stacks) rotatesyms)
+        putStrLn ""
+    where stackStrings = map showStack
+          rotatesyms = concat . repeat $ progressRotateSyms
+          appendRotateSym stack sym = stack ++ "   (" ++ [sym] ++ ")"
 
 sleeptime :: Int
 sleeptime = round 1e6
 
+sleep :: IO ()
+sleep = threadDelay sleeptime
+
+sleepShort :: IO ()
+sleepShort = threadDelay (sleeptime `div` 3)
+
 deleteLine :: String
 deleteLine = "\r\ESC[K"
 
+showWaitDelete :: String -> IO ()
+showWaitDelete stack = do
+                        putStr deleteLine
+                        hFlush stdout
+                        putStr stack
+                        hFlush stdout
+                        sleep
+
 -- Code
 
-showStacks :: [StackCode] -> String
-showStacks stacks = foldl1 (\a e -> a ++ "\n" ++ e) stackstrings
-    where stackstrings = map showStack stacks
+progressRotateSyms :: String
+progressRotateSyms = "-\\|/-|/"
 
 showStack :: StackCode -> String
 showStack ((xs,y,zs),c) = [c] ++ "  " ++ concat (showLeft ++ showCurrent ++ showRight)
     where showLeft = (map (\x -> '[' : show x ++ "]") xs)
           showCurrent = ["{" ++ show y ++ "}"]
           showRight = (map (\z -> '[' : show z ++ "]") zs)
-
-sleep :: IO ()
-sleep = threadDelay sleeptime
 
 interpret :: Code -> IO [StackCode]
 interpret c = liftM (snd . fst) $ runStateT (runWriterT (bfInt c)) emptyStack
@@ -52,3 +64,4 @@ readCode = do
          ln <- getLine
          lns <- readCode
          return (ln++lns)
+
